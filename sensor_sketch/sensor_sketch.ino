6a531f7b12b126112c74a7b4b5ac7fb61d402385
gasolinewaltz <gasolinewaltz@gmail.com>
2016-10-01 18:44:02 -0700

    Initial Commit

---
+++ sensor_sketch/sensor_sketch.ino
@@ -0,0 +1,18 @@
+#include <SD.h>
+#include <SPI.h>
+
+/*
+Dust Sensor Data Logging
+
+SD CARD:
+MISO - pin 50
+MOSI - pin 51 
+SCK  - pin 52
+CS   - pin 45
+
+DUST SENSOR :
+P1 (<= 1μm) - pin 8
+
+*/
+
+
